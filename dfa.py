e28aa4b22208cb27ef0342844a8897ff1fa2e2aa
Adam Kakas <kakasnest@gmail.com>
2021-12-30 14:42:06 +0100

    Parallel composition final refactor done.

--- dfa.py
+++ dfa.py
@@ -37,17 +37,16 @@ class DFA:
 
     #Instance initialization methods below
     def __init_instance(self, data) -> None:
-        self.__states = data["states"]
-        self.__alphabet = data["alphabet"]
+        self.__states=  data["states"]
+        self.__alphabet= data["alphabet"]
         self.__build_transition_cache(data["transitions"])
-        self.__initial_state = data["initial_state"]
-        self.__accepting_states = data["accepting_states"]
+        self.__initial_state= data["initial_state"]
+        self.__accepting_states= data["accepting_states"]
 
     def __build_transition_cache(self, transitions) -> None:
         self.__transitions = {}
         for transition in transitions:
-            self.__transitions[(tuple(transition["from"]), transition["with"])]= transition["to"]
-        print(self.__transitions)
+            self.__transitions[(transition["from"]), transition["with"]]= transition["to"]
 
     #Getters below
     def get_states(self) -> list:
@@ -72,7 +71,7 @@ class DFA:
     def _delta_star(self, user_input:str) -> int:
         state = self.get_initial_state()
         for symbol in user_input:
-            state=self._delta((state, symbol))
+            state= self._delta((state, symbol))
         return state 
     
     def is_accepting_state(self, state:str) -> bool:
@@ -93,7 +92,7 @@ DFA:
 
 
 if __name__ == "__main__":
-    dfa1 = DFA("dfa_after_ac.json", verbose=True)
+    dfa1 = DFA("paralleled_dfa.json", verbose=True)
     while True:
         try:
             user_input = input("\nPress 'Enter' without any other input to quit the application.\nPlease give me a word: ")
--- dfa_utils.py
+++ dfa_utils.py
@@ -11,52 +11,68 @@ class DFA_utils:
         accepting_states= [(x,y) for x in dfa_1.get_accepting_states() for y in dfa_2.get_accepting_states()]
 
         transitions= []
-        for (s1, s2) in states:
+        for s1, s2 in states:
             for symbol in alphabet:
-                    key_1 = (tuple(s1), symbol)
-                    key_2 = (tuple(s2), symbol)
+                    key_1 = (s1, symbol)
+                    key_2 = (s2, symbol)
+                    state_1 = s1
+                    state_2 = s2
+                    transition= {"from": s1+s2, "with": symbol, "to": state_1+state_2}
                            
                     if symbol not in dfa_1.get_alphabet():
                         state_2= dfa_2.get_transitions()[key_2]
-                        transitions.append({"from": (s1,s2), "with": symbol, "to": (s1,state_2)})
+                        transitions.append(transition)
 
                     elif symbol not in dfa_2.get_alphabet():
                         state_1= dfa_1.get_transitions()[key_1]
-                        transitions.append({"from": (s1,s2), "with": symbol, "to": (state_1,s2)})
+                        transitions.append(transition)
 
                     else:
                         state_1= dfa_1.get_transitions()[key_1]
                         state_2= dfa_2.get_transitions()[key_2]
-                        transitions.append({"from": (s1,s2), "with": symbol, "to": (state_1,state_2)})
+                        transitions.append(transition)
 
 
         result= {
-            "states": states,
+            "states": [x+y for x,y in states],
             "alphabet": alphabet,
             "transitions": transitions,
-            "initial_state": initial_state,
-            "accepting_states": accepting_states
+            "initial_state": initial_state[0] + initial_state[1],
+            "accepting_states": [x+y for x,y in accepting_states]
         }
         with open(filename_for_new_dfa, "w") as file:
             dump(result, file, indent=4)
 
     @staticmethod
     def accessible_part(dfa:DFA, filename_for_new_dfa:str="dfa_after_ac.json") -> None:
-        states= []
         transitions= []
-        
-        for _ in range(0, len(dfa.get_states())-2):
-            for state in dfa.get_states():
-                transitions_to_temp= list(map(lambda transition: transition["to"], transitions))
-                for symbol in dfa.get_alphabet():
-                    key= (state, symbol)
-                    transition= {"from": state, "with": symbol, "to": dfa.get_transitions()[key]}
-                    if (state in transitions_to_temp or state == dfa.get_initial_state()):
-                        if state not in states:
-                            states.append(state)
-                        if transition not in transitions:
-                            transitions.append(transition)
 
+        #Append the first transitions
+        for symbol in dfa.get_alphabet():
+            key= (dfa.get_initial_state(), symbol)
+            transition= {"from": dfa.get_initial_state(), "with": symbol, "to": dfa.get_transitions()[key]}
+            transitions.append(transition)
+            print(transitions)
+
+        # for state in dfa.get_states():
+        #     for symbol in dfa.get_alphabet():
+        #         key= (tuple(state), symbol)
+        #         transition= {"from": state, "with": symbol, "to": dfa.get_transitions()[key]}
+        #         if transition not in transitions:
+
+        # for _ in range(0, len(dfa.get_states())-2):
+        #     for state in dfa.get_states():
+        #         transitions_to_temp= list(map(lambda transition: transition["to"], transitions))
+        #         for symbol in dfa.get_alphabet():
+        #             key= (state, symbol)
+        #             transition= {"from": state, "with": symbol, "to": dfa.get_transitions()[key]}
+        #             if (state in transitions_to_temp or state == dfa.get_initial_state()):
+        #                 if state not in states:
+        #                     states.append(state)
+        #                 if transition not in transitions:
+        #                     transitions.append(transition)
+        reachable_states= list(set(map(lambda transition: tuple(transition["to"]), transitions)))
+        states= [dfa.get_initial_state(), *reachable_states]
         accepting_states= list(filter(lambda state: state in states, dfa.get_accepting_states()))
 
         result= {
@@ -69,10 +85,14 @@ class DFA_utils:
         with open(filename_for_new_dfa, "w") as file:
             dump(result, file, indent=4)
 
+        
+
 if __name__ == "__main__":
+    # "./json/dfa_1.json"
+    # "./json/dfa_2.json"
     dfa_1= DFA("./json/dfa_1.json")
     dfa_2= DFA("./json/dfa_2.json")
     DFA_utils.parallel_composition(dfa_1, dfa_2)
-    dfa_3= DFA("paralleled_dfa.json")
+    dfa_3= DFA("paralleled_dfa.json", verbose=True)
     # DFA_utils.accessible_part(dfa_3)
     # dfa_4= DFA("dfa_after_ac.json")
